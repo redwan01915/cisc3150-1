f415de1bdf2eb7b6c1cb5829fb64a8d86e66ecc5
shalvaa26 <shalvaa26@gmail.com>
2018-02-05 16:54:17 -0500

    Fixed Question 5

--- Homeworks/Homework 1/RealTriangle.java
+++ Homeworks/Homework 1/RealTriangle.java
@@ -20,17 +20,28 @@ public class RealTriangle{
 		double b = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
 		double c = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 		
+		//Check to see if the points are collinear (on the same line)
+		double line = (y3 - y2) * x1 + (x2 - x3) * y1 + (x3 * y2 - x2 * y3);
+		
+		if (line == 0)
+			System.out.println("It is a fake triangle");
+		else {
+			
 		//Adding up the angles
 		if ((a+b>c) && (a+c>b) && (b+c>a))
 		System.out.println("It is a real triangle");
-		else 
+		else // It will never come to this since we're inputting coordinates and not the sides
 			System.out.println("It is a fake triangle");
+		}
 	}
 }
 
 /* Test Cases
  * (0,3) (4,5) (6,7)
  * (4.5,2.4) (5.5,7.1) (7.5,2.2)
- * It's impossible to get a fake triangle since we're not inputting the
- * sides of the triangle but the coordinates.
+ * For vertical line: (1,2) (1,3) (1,4)
+ * For horizontal line: (1, 1) (2, 1) (3, 1)
+ * For a diagonal line: (0,0) (2,2) (4,4)
+ * It's impossible to get a fake triangle unless the points are on the same line
+ * since we're not inputting the sides of the triangle but the coordinates.
  */
